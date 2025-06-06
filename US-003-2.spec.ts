c7c960848f2d0ffe7eba763bbd11d8277761d509
Wenderson Artur <150379511+WarturS@users.noreply.github.com>
2025-06-06 08:53:40 -0300

    Update US-003-2.spec.ts

--- US-003-2.spec.ts
+++ US-003-2.spec.ts
@@ -1,21 +1,22 @@
 // APLICAÇÃO: SauceDemo SwagLabs  
-//ALUNO : Wenderson Artur da Silva
+//ALUNO : Wenderson Artur
 //REQUISITO US-003-2 : No carrinho é apresentado um contador com a quantidade de produtos adicionados.
 
 
 import { test, expect } from '@playwright/test';
 
 //=========================================Caminho Feliz===================================================
-test("Verificar se ao selecionar um item , o contador será incremetado (+1)", async ({page}) => {
+test('Verificar se ao selecionar um item , o contador será incremetado (+1)', async ({page}) => {
 
   await page.goto('https://www.saucedemo.com');
   
   test.step('login', async () => {
-// Preenche os campos de login usando page.fill
-await page.fill('[data-test="username"]', 'standard_user');
-await page.fill('[data-test="password"]', 'secret_sauce');
-await page.click('[data-test="login-button"]');
- })
+    // Preenche os campos de login usando page.fill
+    await page.fill('[data-test="username"]', 'standard_user');
+    await page.fill('[data-test="password"]', 'secret_sauce');
+    await page.click('[data-test="login-button"]');
+     })
+
 await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
 
 //Cria uma variável para receber o objeto e logo após verifica se está visível e acrescentou +1
@@ -27,9 +28,10 @@ await expect(contador).toHaveText('1');
 await page.pause();
 });
 
+
 //================================================Teste simulando erro===========================================================
 
-test('Deve exibir corretamente a quantidade de produtos no carrinho', async ({ page }) => {
+test('Deve exibir corretamente a quantidade de produtos no carrinho', async ({ page}) => {
   // Acessa a página da loja
   await page.goto('https://www.saucedemo.com');
 
@@ -52,6 +54,7 @@ test('Deve exibir corretamente a quantidade de produtos no carrinho', async ({ p
   // Simula erro: espera 3 produtos, mas foram adicionados apenas 2
   await expect(contadorCarrinho).toHaveText('3');
 });
+
 
 
 
