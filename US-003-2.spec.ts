7a36559f21875f4d61d6f9ee4ce516e58f784e4d
Wenderson Artur <150379511+WarturS@users.noreply.github.com>
2025-06-06 08:27:34 -0300

    Update US-003-2.spec.ts

--- US-003-2.spec.ts
+++ US-003-2.spec.ts
@@ -1,5 +1,11 @@
+// APLICAÇÃO: SauceDemo SwagLabs  
+//ALUNO : Wenderson Artur da Silva
+//REQUISITO US-003-2 : No carrinho é apresentado um contador com a quantidade de produtos adicionados.
+
+
 import { test, expect } from '@playwright/test';
 
+//=========================================Caminho Feliz===================================================
 test("Verificar se ao selecionar um item , o contador será incremetado (+1)", async ({page}) => {
 
   await page.goto('https://www.saucedemo.com');
@@ -19,5 +25,33 @@ await expect(contador).toHaveText('1');
 
 //Pausar durante a execução do programa
 await page.pause();
-}
-)
+});
+
+//================================================Teste simulando erro===========================================================
+
+test('Deve exibir corretamente a quantidade de produtos no carrinho', async ({ page }) => {
+  // Acessa a página da loja
+  await page.goto('https://www.saucedemo.com');
+
+  test.step('login', async () => {
+    // Preenche os campos de login usando page.fill
+    await page.fill('[data-test="username"]', 'standard_user');
+    await page.fill('[data-test="password"]', 'secret_sauce');
+    await page.click('[data-test="login-button"]');
+     })
+
+  // Clica duas vezes no botão "Adicionar ao carrinho"
+  const botaoAdicionar = page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]');
+  await botaoAdicionar.first().click();
+  const botaoAdicionar2 = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
+  await botaoAdicionar2.first().click();
+
+  // Localiza o contador do carrinho
+  const contadorCarrinho = page.locator('.shopping_cart_badge');
+
+  // Simula erro: espera 3 produtos, mas foram adicionados apenas 2
+  await expect(contadorCarrinho).toHaveText('3');
+});
+
+
+
